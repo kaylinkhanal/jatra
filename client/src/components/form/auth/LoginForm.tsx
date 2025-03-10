cfb33122697e85b81eacda79ccb570878660c92b
rahulcoder2 <rahulchaudharycsit077@gmail.com>
2025-03-10 08:20:16 +0545

    fix: color in login ,register and logo

--- client/src/components/form/auth/LoginForm.tsx
+++ client/src/components/form/auth/LoginForm.tsx
@@ -75,7 +75,7 @@ const LoginForm = () => {
             <p className="text-red-500 text-sm">{formik.errors.password}</p>
           )}
         </div>
-        <Button type="submit" className="w-full" disabled={formik.isSubmitting}>
+        <Button type="submit" className="w-full bg-orange-600!" disabled={formik.isSubmitting}>
           {formik.isSubmitting ? "Logging in..." : "Submit"}
         </Button>
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
