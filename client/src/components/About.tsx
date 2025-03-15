a4de882798aa02b7bec836f2a922b26c1ca4fc76
Rockey C.R <rockeyrai234@gmail.com>
2025-03-15 14:25:08 +0545

    uploading About section video and new background music

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -45,11 +45,13 @@ const About = () => {
 
       <div className="h-dvh max-w-screen" id="clip">
         <div className="mask-clip-path about-image">
-          <img
-            src="auth-1.jpg"
-            alt="Background"
-            className="absolute left-0 top-0 size-full object-cover"
-          />
+        <video
+        src='videos/about.mp4'
+        loop
+        muted
+        autoPlay
+        className="absolute left-0 top-0 size-full object-cover object-center"
+      />
         </div>
       </div>
     </div>
