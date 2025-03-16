53871c63d8e400e96ffa7ce4e93829fcad015ef1
Neha Thakur <tneha6260@gmail.com>
2025-03-17 02:11:01 +0545

    added mapavtar

--- client/src/components/map.tsx
+++ client/src/components/map.tsx
@@ -1,19 +1,26 @@
-'use client'
-import { MapContainer, Marker, Popup, TileLayer, Tooltip } from "react-leaflet"
-import "leaflet/dist/leaflet.css"
-import "leaflet-defaulticon-compatibility"
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+'use client';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+import React from 'react';
+import MapAvatar from './mapavatar';
 
 export default function Map() {
+  const user = {
+    name: 'John Doe',
+    imageUrl: '/avatar.png', 
+  };
+  const userPosition: [number, number] = [27.700769, 85.300140];
 
-
-  return <MapContainer center={[ 27.700769, 85.300140]} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <div className="z-999 absolute right-5">
-    hello
-    </div>
-  </MapContainer>
+  return (
+    <MapContainer center={[27.700769, 85.300140]} zoom={13} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <MapAvatar user={user} position={userPosition} />
+      <div className="z-999 absolute right-5">hello</div>
+    </MapContainer>
+  );
 }
